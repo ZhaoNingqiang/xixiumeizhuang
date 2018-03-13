2db85b2ef2118c3ba19dcb8a7f8cc11bd495f33e
Ningqiang.Zhao <ningqiang.zhao@grabtaxi.com>
2018-03-13 18:52:59 +0800

    goods detail

---
+++ pages/goods/detail/detail.js
@@ -0,0 +1,77 @@
+// pages/goods/detail/detail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    goodsDeatil: {
+      id: 12,
+      name: "氨基酸活氧洁面啫喱",
+      poster: [
+        "../../../products/hufasu.jpg",
+        "../../../products/xifashui.jpg"
+      ],
+      introduce: "../../../products/introduce/ximianru.jpeg",
+      price: 68,
+      passPrice: 68.00,
+      soldCount: "已售12"
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
